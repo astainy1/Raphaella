0c1acc6ce8b538c673b378b7d94eb740fe4e38f9
Gabirel-3191997 <gabrielwkun@gmail.com>
2025-07-20 17:46:24 +0000

    about page added

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -11,8 +11,12 @@ class About extends React.Component{
 
 			<div className="h-96 bg-white">
 
-				<h1>who we are</h1>
+				<div className="pt-20">
+					<h1 className="text-sky-900 font-sans font-bold capitalize text-5xl ml-32">who we are</h1>
+					<br/>
+					<hr className="mx-32"/>
 
+				</div>
 			</div>
 
 			</>

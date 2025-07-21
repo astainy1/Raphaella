b8bc656cc7cd0805934743887619fcc80a196a28
Gabirel-3191997 <gabrielwkun@gmail.com>
2025-07-21 18:18:51 +0000

    missing gallery section

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ class App extends React.Component {
                 <Section />
                 <Content />
                 <SectionContent />
+                <Gallery/>
               </>
             }
           />

bded334f6b7768b733836d5da827f5707a773f21
Gabirel-3191997 <gabrielwkun@gmail.com>
2025-07-23 14:34:36 +0000

    update

--- src/pages/core_values.jsx
+++ src/pages/core_values.jsx
@@ -15,39 +15,39 @@ class CoreValues extends React.Component{
 				
 				<div className="flex flex-wrap justify-center gap-5 md:gap-0 mt-9">
 
-					<div className="w-80 bg-sky-900 h-96 text-white">
+					<div className="w-80 bg-sky-900 h-80 text-white">
 						<h1 className="text-2xl font-bold font-sans capitalize text-white ml-5 mt-5 mx-5">accept</h1>
 						<p className="font-normal font-sans text-white text-lg ml-5 mt-2 h-80">
 							Embracing each individual for who they are, fostering inclusion and understanding.
 						</p>
 					</div>
-					<div className="w-80 bg-sky-800 h-96 text-white">
+					<div className="w-80 bg-sky-800 h-80 text-white">
 						<h1 className="text-2xl font-bold font-sans capitalize text-white ml-5 mt-5 mx-5">inspire</h1>
 						<p className="font-normal font-sans text-white text-lg ml-5 mt-2 h-80">
 							Encouraging families, communities, and individuals to see unique strengths with potential within everyone.
 						</p>
 					</div>
-					<div className="w-80 bg-sky-900 h-96 text-white">
+					<div className="w-80 bg-sky-900 h-80 text-white">
 						<h1 className="text-2xl font-bold font-sans capitalize text-white ml-5 mt-5 mx-5">Nuture</h1>
 						<p className="font-normal font-sans text-white text-lg ml-5 mt-2 h-80">
 							Providing support, education, and resources to help individuals and families thrive.
 						</p>
 					</div>
 				</div>
-				{/*<div className="flex flex-wrap justify-center gap-5 mt-5 md:gap-0 md:mt-0">
-										<div className="w-80 bg-sky-800 h-60 text-white">
+				<div className="flex flex-wrap justify-center gap-5 mt-5 md:gap-0 md:mt-5">
+										<div className="w-80 bg-sky-800 h-80 text-white">
 											<h1 className="text-2xl font-bold font-sans capitalize text-white ml-5 mt-5 mx-5">empower</h1>
 						<p className="font-normal font-sans text-white text-lg ml-5 mt-2 h-80">
-							Embracing each individual for who they are, fostering inclusion and understanding.
+							Equipping individuals and families with the tools and opportunities to overcome challenges and succeed.
 						</p>
 										</div>
-					<div className="w-80 bg-sky-900 h-60 text-white">
+					<div className="w-80 bg-sky-900 h-80 text-white">
 						<h1 className="text-2xl font-bold font-sans capitalize text-white ml-5 mt-5 mx-5">embrace</h1>
 						<p className="font-normal font-sans text-white text-lg ml-5 mt-2 h-80">
-							Embracing each individual for who they are, fostering inclusion and understanding.
+							Celebrating diversity and creating a compassionate,supportive community.
 						</p>
 					</div>
-				</div>*/}
+				</div>
 				<TeamMembers/>
 				</div>
 				
